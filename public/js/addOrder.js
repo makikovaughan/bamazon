b915bc93a26e2e2c49b1a30dd7e80e40f3694c4f
Makiko Vaughan <makikovaughan@Makikos-MacBook-Pro.local>
2018-11-07 13:17:44 -0500

    Updated the production DB setup

--- public/js/addOrder.js
+++ public/js/addOrder.js
@@ -1,13 +1,27 @@
-const addOrder = function(e){
+const addOrder = function (e) {
 
-     //Avoid reset
-     e.preventDefault();
+    //Avoid reset
+    e.preventDefault();
 
-     // Get the stock information
-     const stockTag = $('tbody tr');
+    // Get the stock information
+    // const stockTag = $('tbody tr');
 
-     console.log("Ordertable", stockTag);
+    const stockTag = document.querySelectorAll('tbody tr');
 
-     const newOrder = [];
+    const newOrder = [];
 
+    console.log("Stock tag",stockTag);
+
+    //Get the stock information to add to the array of Object(Orders)
+    stockTag.forEach(function (row) {
+
+        const stockData = row.querySelectorAll("td");
+
+        console.log("Stock:" , stockData);
+
+        const stockQuantity = stockData[3].childNodes[0].value;
+
+        console.log("Stock QTY:", stockQuantity);
+
+    });
 }
